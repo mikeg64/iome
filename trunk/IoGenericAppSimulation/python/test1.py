b1407fb1fe5d415f41182b19cb954a50ca7dc741
mikeg64 <mikeg64@48ea8d0f-b80c-4eb2-8277-5c5df87567b4>
2009-07-15 21:40:44 +0000

    first test

--- trunk/IoGenericAppSimulation/python/test1.py
+++ trunk/IoGenericAppSimulation/python/test1.py
@@ -1,4 +1,7 @@
 import iome
 myvar=[]
-myvar.append('http://localhost:8080')
-iome.ExitIOME(myvar)
+#myvar.append('http://localhost:8080')
+myvar.append('localhost')
+myvar.append(8080)
+myvar.append(0)
+iome.AddIntParam('newvar',25,7,myvar)
