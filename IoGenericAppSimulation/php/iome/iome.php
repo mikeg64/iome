758e68e150dd209a08e631bc2a1d9236c00c494b
mikeg64 <mikeg64@48ea8d0f-b80c-4eb2-8277-5c5df87567b4>
2009-08-14 00:01:44 +0000

    testing multiple method calls and using system to call iogs client request first successful test of soap client call from php

--- IoGenericAppSimulation/php/iome/iome.php
+++ IoGenericAppSimulation/php/iome/iome.php
@@ -1,17 +1,14 @@
 <?php
 
 
-include_once('nusoap.php');
-
 
 class ioservice {
 
     var $server, $port, $id, $method;
 
-    //$method=0 default using system call
-    //$method=1 using nusoap
-    //$method=2 using zend developed client plugin
-    
+     //$method=0 default using system call
+     //$method=1 using zend developed client plugin
+     //$method=2 using nusoap    
 
 }
 
@@ -25,33 +22,18 @@ function setparamdouble( $name, $val , $ioservice)
 	
       $server='http://'.$ioservice->server.':'.$ioservice->port.'/';
 
-      //$client = new nusoap_client($server);
 
-	
-//result is jobid
-      $server='http://'.$ioservice->server.':'.$ioservice->port;
 
       if ($ioservice->method == 1){
-      $client = new nusoap_client($server);
-      
-
-	echo "from setparam server is: {$ioservice->server} and port is {$ioservice->port}.";
-	echo "request is: {$name} val is {$val}";
-
-      
-
-      $id=$ioservice->id;
-	// Call the SOAP method
-      $params = array(  'id' => (int)$id,
-			'name' => $name,
-                        'value' => (float)$val
-                      );
-
-	$result = $client->call('setparamdouble', $params);
+                $request = "setparam double ".$name." ".$val." ".$ioservice->id ." ".$ioservice->port ." ".$ioservice->server ;		
+		//$result=system('iogs getparam double d1');
+		//$result=system('ls');
+                $result = `echo iophp($request)`;
+		//echo "$request set param and result is $result";
 	}
         elseif ($ioservice->method == 0)
 	{
-                $request = "iogs setparam double ".$name." ".$val." ".$ioservice->id ." ".$ioservice->port ." ".$ioservice->server ;		
+                $request = "ioclient setparam double ".$name." ".$val." ".$ioservice->id ." ".$ioservice->port ." ".$ioservice->server ;		
 		//$result=system('iogs getparam double d1');
 		//$result=system('ls');
                 $result=system($request);
