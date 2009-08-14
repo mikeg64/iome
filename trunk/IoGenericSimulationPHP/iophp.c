df7ef6bcb6904ee1762b5a1b1e14e2ae47cad273
mikeg64 <mikeg64@48ea8d0f-b80c-4eb2-8277-5c5df87567b4>
2009-08-14 00:00:44 +0000

    testing multiple method calls and using system to call iogs client request first successful test of soap client call from php

--- trunk/IoGenericSimulationPHP/iophp.c
+++ trunk/IoGenericSimulationPHP/iophp.c
@@ -35,21 +35,43 @@ PHP_FUNCTION(iophp)
 {
     
     char *response;
+    char *fullresponse;
+    char *command = "ioclient ";
+    char *fullrequest;
 
     char *request;
     int s_len;
     double b;
-    
+    char buf[1000];
+    FILE *ptr;
+    zval *zname;
+        
 
-    if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &request, &s_len) == FAILURE) {
+    if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &request,&s_len) == FAILURE) {
         RETURN_NULL();
     }
+  
     
     
+    fullrequest=emalloc(s_len+9);
+    fullresponse=(char *)pemalloc(1000,1);
+    sprintf(fullrequest,"%s%s",command,request);
+    //printf('%s',zname);
 
+    if ((ptr = popen(fullrequest, "r")) != NULL)
+              while (fgets(buf, 1000, ptr) != NULL)
+                              (void) sprintf(fullresponse,"%s", buf);
+    (void) pclose(ptr);
+  
+    
+    /*php_printf("2: %s",fullresponse);*/
+    
+    
+    //fullrequest = strcat(command, request);
+    response = (char *)pestrdup(fullresponse, 1);
+    /*php_printf("3: %s",response);*/
+    RETURN_STRING(fullresponse, 1);
+    //RETVAL_STRINGL(fullresponse, strlen(fullresponse),0);
 
-    response = estrndup(request, s_len);
-    RETURN_STRING(response, 0);
-
-    //RETURN_STRING("iophp", 1);
+    //RETURN_STRING("iophp done", 1);
 }
