fe2e7946121fb843f27e1434ddb65296ebe213a8
mikeg64 <mikeg64@48ea8d0f-b80c-4eb2-8277-5c5df87567b4>
2009-09-24 09:57:50 +0000

    problems using ftp in matlab under the 64 bit linux matlab r2009a version meant that the scripts had to be altered to enable ftp to behave correctly

--- trunk/src/tools/genericappsimulation/matlab/caiman-ws/iogenericsim_sge.sh
+++ trunk/src/tools/genericappsimulation/matlab/caiman-ws/iogenericsim_sge.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
 #$ -q short.q
 
-IOME_SIMNAME="mysim"
-#iogs initiome $IOME_SIMNAME null  &
-iogs initiome $IOME_SIMNAME null  &
-#ioapp genericappmat.sh ../caimansaasexample.m mysim nullmatlab -nosplash -nodisplay -r ../caimansaasexample.m
+IOME_SIMNAME="mysim"
+
+iogs initiome null $IOME_SIMNAME null  &
+
+IOME_WSPORT=`cat ${IOME_SIMNAME}0_port.txt`
+echo port is $IOME_WSPORT
+
+cp ../caimansaasexample.m .
+cp ../cpanellogin.sh .
+
+iogs readsimulation simfile.xml 0 $IOME_WSPORT localhost
+IMFILE=`iogs getparam string imagefile 0 $IOME_WSPORT localhost`
+echo "s/%imagefile%/$IMFILE/" > sed.in
+sed -f sed.in cpanellogin.sh > tmp && mv tmp cpanellogin.sh
+
+ftp -i cpaneldev.shef.ac.uk < cpanellogin.sh
+
+#ioapp genericappmat.sh ../caimansaasexample.m mysim null
+matlab -nosplash -nodisplay -r caimansaasexample
