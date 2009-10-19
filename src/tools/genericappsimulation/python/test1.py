ae76c9d239c3d0ce62326289971d6c446f8e59d9
mikeg64 <mikeg64@48ea8d0f-b80c-4eb2-8277-5c5df87567b4>
2009-10-19 21:55:10 +0000

    the following get,set,add funcs tested OK int,float,string,vec

--- src/tools/genericappsimulation/python/test1.py
+++ src/tools/genericappsimulation/python/test1.py
@@ -4,4 +4,25 @@ myvar=[]
 myvar.append('localhost')
 myvar.append(8080)
 myvar.append(0)
-iome.AddIntParam('newvar',25,7,myvar)
+
+iome.AddIntParam('i1',25,7,myvar)
+res=iome.GetIntParam('i1',myvar)
+print res;
+
+iome.AddFloatParam('d1',25.123456789,7,myvar)
+res=iome.GetFloatParam('d1',myvar)
+print res;
+
+iome.AddStringParam('s1','test string',7,myvar)
+res=iome.GetStringParam('s1',myvar)
+print res;
+
+
+myvec=[]
+myvec.append(0.1)
+myvec.append(0.2)
+myvec.append(0.3)
+iome.AddVecParam('newvec2',myvec,3,7,myvar)
+
+myvec=iome.GetVecParam('newvec2',3,myvar)
+print myvec
