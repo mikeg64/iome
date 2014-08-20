8c879e464797b8f0c5d4e2cfad0e18576c70ef41
mikeg64 <mikeg64@48ea8d0f-b80c-4eb2-8277-5c5df87567b4>
2014-08-20 20:56:17 +0000

    test

--- trunk/src/examples/course/pytho/basics_whileandfor/twhile1.py
+++ trunk/src/examples/course/pytho/basics_whileandfor/twhile1.py
@@ -1,17 +1,17 @@
-% example while loop with a break
-% 
+# example while loop with a break
+#
 try1 = 1;
-while try1
-  n = input( ' Enter a number: ' ) ;
-  if  n < 0.5
-    disp 'Too small !'
-  elseif n > 100
-    disp ' Too big !'
-  else
-     disp ' It will do.'
-%    break
-% the below line will also work for terminating the loop
-     try1 = 0;
-  end
-end
+while try1 ==1:
+	n = input( ' Enter a number: ' ) ;
+	if  n < 0.5:
+		print 'Too small !'
+	elif n > 100:
+		print ' Too big !'
+	else :
+		print ' It will do.'
+#    break
+# the below line will also work for terminating the loop
+# the while is terminated by the final tab
+	try1 = 0;
+
 
--- trunk/src/examples/course/pytho/basics_whileandfor/twhile2.py
+++ trunk/src/examples/course/pytho/basics_whileandfor/twhile2.py
@@ -1,17 +1,18 @@
-% example while statement and example prompt for input via disp command.
-% 
+#simple python script
+#example while statement and example prompt for input via disp command.
+
 try1 = 1;
 prompt2 = 'Try Again ! ';
-while try1
-  n = input( ' Enter a number: ' ) ;
-  if  n < 0.5
-     disp 'Too small !';
-     disp ( prompt2);
-  elseif n > 100
-     disp ' Too big !'
-     disp ( prompt2);
-  else
-     disp ' It will do.'
-     break
-  end
-end
+while try1==1:
+	n = input( ' Enter a number: ' ) ;
+	if  n < 0.5:
+		print 'Too small !';
+		print ( prompt2);
+	elif n > 100:
+		print ' Too big !'
+		print ( prompt2);
+	else :
+		print ' It will do.'
+		try1=0
+	
+     
