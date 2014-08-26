d710ef69468797a7fc7bf38cc2f2876e416de841
mikeg64 <mikeg64@48ea8d0f-b80c-4eb2-8277-5c5df87567b4>
2014-08-26 08:54:51 +0000

    newton

--- trunk/src/examples/course/pytho/root_newton/plotroot.py
+++ trunk/src/examples/course/pytho/root_newton/plotroot.py
@@ -1,4 +1,9 @@
+from numpy import *
+from os import *
 
+import matplotlib.pyplot as plt
+import numpy as np
+import math
 
 
 # This is an example use of the plot function 
@@ -6,35 +11,31 @@
 # problem solved earlier.
 # % a is the lower value of the range
 # b is the upper value of the range
+eps=0.0000001
+inf=9.9e30
+a = 0.0
+fa = -inf
+x = 3.0
+fb = inf
+b=x
+delta = (b-a)/100.0
 
-a = 0;fa = -%inf;
-x = 3;fb = %inf;
-b=x;
-delta = (b-a)/100;
-xx = a:delta:b;
-plot(xx,xx .^3-2*xx-5);
-set(gca(),"auto_clear","off");
-# !! L.13: Matlab function line not yet converted, original calling sequence used.
-#hax = line([a,b],[0,0]);
-# !! L.14: Matlab function set not yet converted, original calling sequence used.
-# L.14: (Warning name conflict: function name changed from set to %set).
-#%set(hax,"Color","red");
-while (sqrt((x-a)^2))>0.0001
+xx = np.arange(a,b,delta)
+asize=xx.size
+yy = np.zeros(asize)
+for i in range(1,asize):
+    yy[i]=0.5*sin(2*(xx[i]-(math.pi/4)))+0.5*sin(xx[i]);
 
+
+
+while (sqrt(math.pow(x-a,2)))>eps:
   a=x;
-  #fx = x^3-2*x-5;
-  #dfx= 3*x^2-2;
-  
-  fx = 0.5*sin(2*(x-(%pi/4)))+0.5*sin(x);
-  dfx= cos(2*(x-(%pi/4)))+0.5*cos(x);
+  fx = 0.5*sin(2*(x-(math.pi/4)))+0.5*sin(x);
+  dfx= cos(2*(x-(math.pi/4)))+0.5*cos(x);
   x=a-(fx/dfx);
-  #halt;
-   
-  plot(x,fx,"o")
-  
-  disp(x-a);
-end;
+  print x-a
+ 
 
-disp(" The root is :");
-disp(x);
-set(gca(),"auto_clear","on");
+
+print ' The root is :',x
+plt.plot(xx,yy)
