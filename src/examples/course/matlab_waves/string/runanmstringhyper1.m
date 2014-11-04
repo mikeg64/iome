105ff472c92205fcf039cf3d4893dcbb9e338e57
mikeg64 <mikeg64@48ea8d0f-b80c-4eb2-8277-5c5df87567b4>
2014-11-04 22:26:10 +0000

    hyperdiffusion model starting to work with shock diffusion coefficient

--- src/examples/course/matlab_waves/string/runanmstringhyper1.m
+++ src/examples/course/matlab_waves/string/runanmstringhyper1.m
@@ -90,6 +90,8 @@ wavespeed=10
 %courant = (wavespeed*dt)/dx;
 nt=10000;
 hmax=3;
+chyp=0.00005;
+cshk=1;
 
 %parameters to define width of propagating shape
 n1=75;
@@ -105,6 +107,26 @@ tv = zeros(ni,1);
 uh = zeros(ni,1);
 vh = zeros(ni,1);
 visc = zeros(ni,1);
+
+%hyperviscosity
+nur = zeros(ni,1);
+nul = zeros(ni,1);
+nushk = zeros(ni,1);
+
+d3l = zeros(ni,1);
+d3r = zeros(ni,1);
+
+d1l = zeros(ni,1);
+d1r = zeros(ni,1);
+
+md3l = zeros(ni,1);
+md3r = zeros(ni,1);
+
+md1l = zeros(ni,1);
+md1r = zeros(ni,1);
+
+dudx = zeros(ni,1);
+
 
 %u((ni+1)/2)=b;
   for i = 1:ni
@@ -128,10 +150,10 @@ visc = zeros(ni,1);
         ymax=1.2*hmax;
         ymin=-0.2*hmax;
       %remove comments below to set up a sine wave  
-      %  if (i>n1) && (i<n3)
-      %    u(i)=hmax*sin(2*pi*(i-n1)/((n3-n1)));
-      %     ymin=-1.2*hmax;
-      %  end
+        if (i>n1) && (i<n3)
+          u(i)=hmax*sin(2*pi*(i-n1)/((n3-n1)));
+           ymin=-1.2*hmax;
+        end
       
  
 
@@ -153,6 +175,8 @@ vw=0.1;
 uold=u;
 
 
+
+
 for n = 1:nt
    %dt=0.001; 
    t=n*dt;
@@ -161,7 +185,7 @@ for n = 1:nt
     
     
     set(h,'YData',u);
-    set(gca,'YLim',[ymin ymax]);
+    set(gca,'YLim',[4*ymin 2*ymax]);
 
      %first order 
      starti=2;
@@ -176,14 +200,19 @@ for n = 1:nt
     %comment the above     
     %for i = 3:ni-2
       %Lax-Wendroff   
-     v(i) = u(i)+c*(u(i+1)-u(i-1))/2+c^2*(u(i+1)-2*u(i)+u(i-1))/2;
+     %v(i) = u(i)+c*(u(i+1)-u(i-1))/2+c^2*(u(i+1)-2*u(i)+u(i-1))/2;
       
       
-      %Crank-Nicholson (average of forward and backward difference)
-      v(i) = u(i)+c*(u(i+1)-u(i-1));
+     
       %first order central differencing
-      %v(i) = u(i)+c*(u(i+1)-u(i-1))/2;%+c*(uold(i+1)-uold(i-1))/2;
+      dudx(i)=(u(i+1)-u(i-1))/(2*dx);
+      v(i) = u(i)+c*(u(i+1)-u(i-1))/2;%+c*(uold(i+1)-uold(i-1))/2;
  
+      hdx(i)=(c.^2)*(  (nur(i)+nushk(i))*(u(i+1)-u(i))     -nul(i)*(u(i)-u(i-1))  )/(2);
+           %correction with hyperdiffusion term
+      v(i)=v(i)+(c.^2)*(  (nur(i)+nushk(i))*(u(i+1)-u(i))     -nul(i)*(u(i)-u(i-1))  )/(2);
+
+      
        %second order central differencing %includes Lax-Wedroff correction
        %term
        %v(i) = u(i)+c*(u(i-2)+8*u(i+1)-8*u(i-1)-u(i+2))/12;%+c^2*(u(i+1)-2*u(i)+u(i-1))/2;
@@ -196,7 +225,8 @@ for n = 1:nt
          u(i)=v(i);
       end;
       
-  %1st order periodic boundary condition
+      
+      %1st order periodic boundary condition
   u(ni)=u(2);
   u(1)=u(ni-1);
   
@@ -205,9 +235,84 @@ for n = 1:nt
   %u(2)=u(ni-2);
   %u(ni)=u(4);
   %u(1)=u(ni-3);
-  
-  
       
+         %second order %uncomment these to test second order differencing
+     starti=3;
+     finishi=ni-2;
+     
+      %Compute hyperdiffusion coefficients
+      for i = starti:finishi
+         d3l(i)=3*u(i)-u(i-1)-u(i+1);
+         d3r(i)=3*u(i+1)-u(i)-u(i-1);
+         
+         d1l(i)=u(i)-u(i-1);
+         d1r(i)=u(i+1)-u(i);
+      end; 
+ 
+        %2nd order periodic boundary condition
+        d3l(ni-1)=d3l(3);
+        d3l(2)=d3l(ni-2);
+        d3l(ni)=d3l(4);
+        d3l(1)=d3l(ni-3);
+        
+        d3r(ni-1)=d3r(3);
+        d3r(2)=d3r(ni-2);
+        d3r(ni)=d3r(4);
+        d3r(1)=d3r(ni-3);
+
+        d1l(ni-1)=d1l(3);
+        d1l(2)=d1l(ni-2);
+        d1l(ni)=d1l(4);
+        d1l(1)=d1l(ni-3);
+        
+        d1r(ni-1)=d1r(3);
+        d1r(2)=d1r(ni-2);
+        d1r(ni)=d1r(4);
+        d1r(1)=d1r(ni-3);      
+        
+     starti=2;
+     finishi=ni-3;
+      for i = starti:finishi
+         md3l(i)=max(d3l(i:i+2));
+         md1l(i)=max(d1l(i-1:i+3));
+         md3r(i)=max(d3r(i:i+2));
+         md1r(i)=max(d1r(i-1:i+3));
+        
+      end; 
+      
+         %first order 
+     starti=2;
+     finishi=ni-1;  
+      for i = starti:finishi
+         if md1r(i)>0
+             nur(i)=wavespeed*chyp*md3r(i)/(md1r(i)*dx);             
+         else
+             nur(i)=0;
+         end;
+         
+         if md1l(i)>0
+             nul(i)=wavespeed*chyp*md3l(i)/(md1l(i)*dx);
+         else
+             nul(i)=0;
+         end;        
+      end;     
+  
+       %1st order periodic boundary condition
+       nur(ni)=nur(2);
+       nur(1)=nur(ni-1);
+       nul(ni)=nul(2);
+       nul(1)=nul(ni-1);  
+  
+      for i = starti:finishi
+         
+             nushk(i)=abs(cshk*dudx(i)*(dx.^2));             
+               
+      end;        
+       
+        nushk(ni)=nushk(2);
+       nushk(1)=nushk(ni-1);
+      
+       
  
 drawnow;
 end;
